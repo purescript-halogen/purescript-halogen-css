71a887d6c336a2640d391e0f31348b4501b36962
Maxim Zimaliev <zimaliev@gmail.com>
2017-02-07 00:09:45 +0300

    update for halogen 1.0.0

--- src/Halogen/HTML/CSS.purs
+++ src/Halogen/HTML/CSS.purs
@@ -15,24 +15,15 @@ import Data.Array (mapMaybe, concatMap, singleton)
 import Data.Either (Either)
 import Data.Foldable (foldMap)
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Newtype (class Newtype)
+import Data.MediaType (MediaType(..))
 import Data.StrMap as SM
 import Data.String (joinWith)
 import Data.Tuple (Tuple(..))
 
 import Halogen.HTML as HH
-import Halogen.HTML.Core (HTML, Prop, class IsProp, prop, propName, attrName)
 import Halogen.HTML.Elements as HE
-import Halogen.HTML.Properties as P
-
--- | A newtype for CSS styles
-newtype Styles = Styles (SM.StrMap String)
-
-derive instance newtypeStyles ∷ Newtype Styles _
-
-instance stylesIsProp ∷ IsProp Styles where
-  toPropString _ _ (Styles m) =
-    joinWith "; " $ SM.foldMap (\key value → [key <> ": " <> value]) m
+import Halogen.HTML.Properties as HP
+import Halogen.HTML.Core as HC
 
 -- | Render a set of rules as an inline style.
 -- |
@@ -43,13 +34,16 @@ instance stylesIsProp ∷ IsProp Styles where
 -- |                      display block ]
 -- |       [ ... ]
 -- | ```
-style ∷ ∀ i. CSS → Prop i
+style ∷ ∀ i r. CSS → HP.IProp (style ∷ String|r) i
 style =
-  prop (propName "style") (Just $ attrName "style")
-    <<< Styles
+  HP.prop (HC.PropName "style")
+    <<< toString
     <<< rules
     <<< runS
   where
+  toString ∷ SM.StrMap String → String
+  toString = joinWith "; " <<< SM.foldMap (\key val → [ key <> ": " <> val])
+
   rules ∷ Array Rule → SM.StrMap String
   rules rs = SM.fromFoldable properties
     where
@@ -64,8 +58,8 @@ style =
   rights = concatMap $ foldMap singleton
 
 -- | Render a set of rules as a `style` element.
-stylesheet ∷ ∀ p i. CSS → HTML p i
+stylesheet ∷ ∀ p i. CSS → HC.HTML p i
 stylesheet css =
-  HE.style [ P.type_ "text/css" ] [ HH.text content ]
+  HE.style [ HP.type_ $ MediaType "text/css" ] [ HH.text content ]
   where
   content = fromMaybe "" $ renderedSheet $ render css
